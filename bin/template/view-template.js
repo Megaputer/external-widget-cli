599d702d3a9acf10259f528d514d7dfbbfa21905
Roman Ivanov <rivanov@megaputer.ru>
2024-10-17 15:57:48 +0300

    Use correct EOL

--- bin/template/view-template.js
+++ bin/template/view-template.js
@@ -17,7 +17,7 @@ export const ${options.name}: React.FC<Props> = ({ requestor }) => {
   React.useEffect(() => {
     const fetchData = async () => {
       const guid = wrapperGuid.current = await requestor.wrapperGuid();
-      let dsInfo = await requestor.info(guid);
+      const dsInfo = await requestor.info(guid);
 
       setColumnCount(dsInfo.columns.length);
       setRowCount(dsInfo.rowCount);
@@ -30,15 +30,17 @@ export const ${options.name}: React.FC<Props> = ({ requestor }) => {
       setValues(values);
     };
     fetchData();
-
   }, [requestor]);
 
   return (
     <div className='main'>
-      <div className='column'>Data: {rowColumn} column(s)</div>
-      <div className='row'>Data: {rowCount} row(s)</div>
+      <div className='column'>
+        {'Data: ' + rowColumn + ' column(s)'}
+      </div>
+      <div className='row'>
+        {'Data: ' + rowCount + ' row(s)'}
+      </div>
     </div>
   );
-}
-`);
+};`);
 }
